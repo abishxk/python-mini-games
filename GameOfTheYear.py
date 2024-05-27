51b7b4362dfcce779b6ddbf9fad7636a47ac17ef
abishxk <107830484+abishxk@users.noreply.github.com>
2024-05-27 12:19:39 +0530

    Add files via upload

---
+++ GameOfTheYear.py
@@ -0,0 +1,40 @@
+from opcode import hasnargs
+import pyautogui as py
+
+py.press("win")
+py.moveTo(1274,961)
+py.click(1274,961)
+py.moveTo(1270,863)
+for i in range(69):
+    py.click(1270,863)
+#print(py.position())
+#hans
+#hasnargshans
+#hasnargshans
+#hasnargshans
+#hasnargshans
+#hasnargshans
+#hasnargshans
+#hasnargshans
+#hasnargshans
+#hasnargshans
+#hans
+#hasnargshans
+#hasnargshans
+#hasnargshans
+#hasnargshans
+#hasnargshans
+#hasnargshans
+#hasnargshans
+#hasnargshans
+#hasnargshans
+#hans
+#hasnargshans
+#hasnargshans
+#hasnargshans
+#hasnargshans
+#hasnargshans
+#hasnargshans
+#hasnargshans
+#hasnargshans
+#hasnargshans
